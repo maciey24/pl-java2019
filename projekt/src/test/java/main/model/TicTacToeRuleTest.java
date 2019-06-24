4c3f17ec5610bba65796c47bc0dc84eece172d64
Maciej Ślusarz <189789@edu.p.lodz.pl>
2019-06-25 00:12:26 +0200

    tests

--- projekt/src/test/java/main/model/TicTacToeRuleTest.java
+++ projekt/src/test/java/main/model/TicTacToeRuleTest.java
@@ -1,5 +1,7 @@
 package main.model;
 
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 import main.controller.Window;
 import org.junit.jupiter.api.BeforeAll;
@@ -31,6 +33,118 @@ class TicTacToeRuleTest {
 
     @Test
     void checkWhoWon() {
-        Board board = new Board();
+        Board board = new Board(window);
+        TicTacToeRule ticTacToeRule = new TicTacToeRule();
+
+        Shape whoWon = ticTacToeRule.checkWhoWon(board);
+
+        assertNull(whoWon);
+    }
+
+    @Test
+    void checkWhoWon2() {
+        Board board = new Board(window);
+        TicTacToeRule ticTacToeRule = new TicTacToeRule();
+        GridIndex gridIndex = new GridIndex(0, 0);
+        board.setElement(gridIndex, new Circle());
+
+        Shape whoWon = ticTacToeRule.checkWhoWon(board);
+
+        assertNull(whoWon);
+    }
+
+    @Test
+    void checkWhoWon3() {
+        Board board = new Board(window);
+        TicTacToeRule ticTacToeRule = new TicTacToeRule();
+        GridIndex gridIndex = new GridIndex(0, 0);
+        board.setElement(gridIndex, new Circle());
+        gridIndex = new GridIndex(1, 0);
+        board.setElement(gridIndex, new Circle());
+        gridIndex = new GridIndex(2, 0);
+        board.setElement(gridIndex, new Circle());
+
+        Shape whoWon = ticTacToeRule.checkWhoWon(board);
+
+        assertEquals(whoWon.getClass(), Circle.class);
+    }
+
+    @Test
+    void checkWhoWon4() {
+        Board board = new Board(window);
+        TicTacToeRule ticTacToeRule = new TicTacToeRule();
+        GridIndex gridIndex = new GridIndex(0, 0);
+        board.setElement(gridIndex, new Rectangle());
+        gridIndex = new GridIndex(1, 1);
+        board.setElement(gridIndex, new Rectangle());
+        gridIndex = new GridIndex(2, 2);
+        board.setElement(gridIndex, new Rectangle());
+
+        Shape whoWon = ticTacToeRule.checkWhoWon(board);
+
+        assertEquals(whoWon.getClass(), Rectangle.class);
+    }
+
+    @Test
+    void checkWhoWon5() {
+        Board board = new Board(window);
+        TicTacToeRule ticTacToeRule = new TicTacToeRule();
+        GridIndex gridIndex = new GridIndex(0, 2);
+        board.setElement(gridIndex, new Rectangle());
+        gridIndex = new GridIndex(1, 1);
+        board.setElement(gridIndex, new Rectangle());
+        gridIndex = new GridIndex(2, 0);
+        board.setElement(gridIndex, new Rectangle());
+
+        Shape whoWon = ticTacToeRule.checkWhoWon(board);
+
+        assertEquals(whoWon.getClass(), Rectangle.class);
+    }
+    @Test
+    void checkWhoWon6() {
+        Board board = new Board(window);
+        TicTacToeRule ticTacToeRule = new TicTacToeRule();
+        GridIndex gridIndex = new GridIndex(0, 2);
+        board.setElement(gridIndex, new Rectangle());
+        gridIndex = new GridIndex(1, 1);
+        board.setElement(gridIndex, new Rectangle());
+        gridIndex = new GridIndex(2, 0);
+        board.setElement(gridIndex, new Rectangle());
+        gridIndex = new GridIndex(1, 0);
+        board.setElement(gridIndex, new Circle());
+
+        Shape whoWon = ticTacToeRule.checkWhoWon(board);
+
+        assertEquals(whoWon.getClass(), Rectangle.class);
+    }
+    @Test
+    void checkWhoWon7() {
+        Board board = new Board(window);
+        TicTacToeRule ticTacToeRule = new TicTacToeRule();
+        GridIndex gridIndex = new GridIndex(0, 0);
+        board.setElement(gridIndex, new Circle());
+        gridIndex = new GridIndex(0, 1);
+        board.setElement(gridIndex, new Circle());
+        gridIndex = new GridIndex(0, 2);
+        board.setElement(gridIndex, new Circle());
+
+        Shape whoWon = ticTacToeRule.checkWhoWon(board);
+
+        assertEquals(whoWon.getClass(), Circle.class);
+    }
+    @Test
+    void checkWhoWon8() {
+        Board board = new Board(window);
+        TicTacToeRule ticTacToeRule = new TicTacToeRule();
+        GridIndex gridIndex = new GridIndex(0, 0);
+        board.setElement(gridIndex, new Circle());
+//        gridIndex = new GridIndex(0, 1);
+//        board.setElement(gridIndex, new Circle());
+        gridIndex = new GridIndex(0, 2);
+        board.setElement(gridIndex, new Circle());
+
+        Shape whoWon = ticTacToeRule.checkWhoWon(board);
+
+        assertNull(whoWon);
     }
 }
